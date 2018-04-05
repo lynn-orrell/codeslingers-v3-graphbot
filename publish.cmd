a0306f008ba98a4d9e0ce7d689f5c631c0ee3bb5
Lynn Orrell <lyorrell@microsoft.com>
2018-04-04 21:22:48 -0500

    Initializing repo

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=codeslingers-graphbot-Web-Deploy.pubxml -p:Password=3GG2t4Dq8BLPoos3u2tofJd8szb2w2bxDry9r3hHvz1h3hPepPp5Ldh58Xha
+
